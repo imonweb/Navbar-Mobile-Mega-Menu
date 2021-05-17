dc1cb82e8c9db991dfd2ace63bb29355000225b9
imonweb <contact@imonweb.co.uk>
2021-05-17 17:19:14 +0100

    Initial setup

---
+++ readme.md
@@ -0,0 +1,1 @@
+Navbar Moblie Mega Menu
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const postCSSPlugins = [
+  require('postcss-import'),
+  require('postcss-mixins'),
+  require('postcss-simple-vars'),
+  require('postcss-nested'),
+  require('autoprefixer')
+]
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname,'./dist'),
+    filename: 'myBundle.js'
+  },
+  devServer: {
+    before: function(src, server){
+      server._watch('./src/*.html')
+    },
+    contentBase: path.join(__dirname,'src'),
+    hot: true,
+    port: 3000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader","css-loader", {
+          loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: postCSSPlugins
+              }
+            }
+        }]
+      },
+    ]
+  }
+}
